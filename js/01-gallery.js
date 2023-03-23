738a80bb998ac72e0737dd2ffb99083a522c5a3d
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-03-23 20:23:12 +0100

    task 1 update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,14 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallerySet = document.querySelector(".gallery");
+
+const galleryProject = galleryItems
+  .map(
+    (image) =>
+      `<li><img class = gallery-item src = ${image.preview} alt = ${image.description} width = 400 </img> </li>`
+  )
+  .join("");
+
+gallerySet.insertAdjacentHTML("afterbegin", galleryProject);
