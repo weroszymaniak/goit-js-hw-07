59436a94edc51f5ccbb3584d5908e6c6a04c164c
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-03-25 18:28:42 +0100

    task1 update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,8 +7,18 @@ const gallerySet = document.querySelector(".gallery");
 const galleryProject = galleryItems
   .map(
     (image) =>
-      `<li><img class = gallery-item src = ${image.preview} alt = ${image.description} width = 400 </img> </li>`
+      `<div class = gallery__item><img class = gallery__image src = ${image.preview} 
+      alt = ${image.description} data-source = ${image.original} width = 400 </img> </div>`
   )
   .join("");
 
+function onClick(event) {
+  event.preventDefault();
+  const biggerImg = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  biggerImg.show();
+}
 gallerySet.insertAdjacentHTML("afterbegin", galleryProject);
+
+gallerySet.addEventListener("click", onClick);
