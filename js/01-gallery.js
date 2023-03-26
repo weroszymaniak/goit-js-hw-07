fc3c1eb704c4b772c4a556953aa9b34ec9677851
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-03-26 18:15:30 +0200

    simplelightbox update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,8 +7,8 @@ const gallerySet = document.querySelector(".gallery");
 const galleryProject = galleryItems
   .map(
     (image) =>
-      `<div class = gallery__item><img class = gallery__image src = ${image.preview} 
-      alt = ${image.description} data-source = ${image.original} width = 400 </img> </div>`
+      `<div class = gallery__item> <a class="gallery__link" href="${image.original}"> <img class = "gallery__image" src = "${image.preview}" 
+      alt = "${image.description}" data-source = "${image.original}" /> </a> </div>`
   )
   .join("");
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallerySet = document.querySelector(".gallery");
+
+const galleryProject = galleryItems
+  .map(
+    (image) =>
+      `<a class="gallery__item" href="${image.original}"> <img class = "gallery__image" src = "${image.preview}" 
+      alt = "${image.description}" data-source = "${image.original}" /> </a> </div>`
+  )
+  .join("");
+
+gallerySet.insertAdjacentHTML("afterbegin", galleryProject);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
